5d34fba8689b7531875ebea25626877490f53e6c
Scott Smitelli <ssmitelli@titan-ubuntu.lan>
2015-09-20 19:55:25 -0400

    This thing works!

---
+++ cardset.py
@@ -0,0 +1,25 @@
+import gspread
+import json
+from oauth2client.client import SignedJwtAssertionCredentials
+from render import render_plot
+
+DOC_NAME = 'Plot Twist - Print and Play Card List - v0.1'
+PLOT_NAME = 'Plot Cards'
+TROPE_NAME = 'Trope Cards'
+
+json_key = json.load(open('credentials.json'))
+credentials = SignedJwtAssertionCredentials(
+    json_key['client_email'], json_key['private_key'],
+    ['https://spreadsheets.google.com/feeds'])
+gc = gspread.authorize(credentials)
+
+doc = gc.open(DOC_NAME)
+for sheet in doc.worksheets():
+    data = sheet.get_all_values()
+
+    if sheet.title == PLOT_NAME:
+        card_data = [r[0] for r in data]
+        render_plot(card_data)
+
+    elif sheet.title == TROPE_NAME:
+        card_data = [{'top': r[0], 'mid': r[1], 'bot': r[2]} for r in data]
---
+++ render.py
@@ -0,0 +1,73 @@
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.pagesizes import letter, inch
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import Image, Paragraph, SimpleDocTemplate, Table, TableStyle
+
+
+def render_plot(data):
+    _render(data, 'plot.pdf', 4)
+
+
+def _render(data, filename, width):
+    styles = getSampleStyleSheet()
+    s = styles['Normal']
+    s.alignment = TA_CENTER
+    s.fontName = 'TheSansBP'
+    s.fontSize = 12.96
+    s.leading = 14.64
+    s.textColor = colors.black
+
+    icon = Image('resources/icon_plottwist2.png')
+    icon.drawWidth = 0.556 * inch
+    icon.drawHeight = 0.233 * inch
+
+    pdfmetrics.registerFont(TTFont('TheSansBP', 'resources/TheSans-BoldPlain.ttf'))
+
+    doc = SimpleDocTemplate(
+        filename,
+        pagesize=letter,
+        leftMargin=0.293 * inch,
+        rightMargin=0.293 * inch,
+        topMargin=0.188 * inch,
+        bottomMargin=0)
+
+    def layout(text):
+        it = Table(
+            [
+                [Paragraph(text, s)],
+                [icon]
+            ],
+            colWidths=(1.97 * inch),
+            rowHeights=(1.844 * inch, 0.233 * inch))
+
+        it.setStyle(TableStyle([
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+            ('LEFTPADDING', (0, 0), (-1, -1), 0.15 * inch),
+            ('RIGHTPADDING', (0, 0), (-1, -1), 0.15 * inch),
+            ('TOPPADDING', (0, 0), (-1, -1), 0.117 * inch),
+            ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
+            # Logo stuff
+            ('VALIGN', (0, 1), (0, 1), 'BOTTOM'),
+            ('BOTTOMPADDING', (0, 1), (0, 1), 0.063 * inch),
+        ]))
+
+        return it
+
+    data = [layout(x) for x in data]
+    rows = [data[x:x + width] for x in xrange(0, len(data), width)]
+
+    t = Table(rows)
+
+    t.setStyle(TableStyle([
+        ('LEFTPADDING', (0, 0), (-1, -1), 0),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 0),
+        ('TOPPADDING', (0, 0), (-1, -1), 0),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
+        ('GRID', (0, 0), (-1, -1), 0.02 * inch, (0.341, 0.341, 0.341))
+    ]))
+
+    doc.build([t])
